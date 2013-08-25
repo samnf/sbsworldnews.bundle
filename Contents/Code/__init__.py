441072d2a5061d56c29a3b799f8ece3e91721738
Sam Fulton <sam.fulton@vigorsystemsinc.com>
2013-08-24 20:56:21 -0700

    removed unnecessary menu structure, only showing full episodes for q=World%20News%20Australia

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -24,87 +24,28 @@ def VideoMainMenu():
     dir = MediaContainer(viewGroup="InfoList")
     content = GetContent()
     #Log("Content>>" + content)
-    for channel in GetChannels(content):
-        dir.Append(Function(DirectoryItem(Lvl2, title=channel), key=channel, content=content))
-    for genre in GetGenres(content):
-        dir.Append(Function(DirectoryItem(Lvl2, title=genre), key=genre, content=content))   
     return dir
 
-def Lvl2(sender, key, content):
-    dir = MediaContainer(viewGroup="InfoList", title2=key)
-    for show in content:
-        for genre in show['genres']:
-            if genre == key:
-                temp = re.sub(' ','-',show['programName'])
-                #url=BASE_URL+show['ID']+'/'+re.sub('-+','-',temp)
-                Log('For show '+ show['name'] + ' adding URL: ' + show['url'])
-                dir.Append(WebVideoItem(show['url'], title=show['name'], subtitle='runtime: '+ str(int(show['duration']/60)) +' mins.', thumb=show['thumbnailURL'], summary=show['description']))
-        for channel in show['channels']:
-            if channel == key:
-                temp = re.sub(' ','-',show['programName'])
-                #url=BASE_URL+show['ID']+'/'+re.sub('-+','-',temp)
-                Log('For show '+ show['name'] + ' adding URL: ' + show['url'])
-                try:
-                    dir.Append(WebVideoItem(show['url'], title=show['name'], subtitle='runtime: '+ str(int(show['duration']/60)) +' mins.', thumb=show['thumbnailURL'], summary=show['description']))
-                except:
-                    Log('failure to add web video for : ' + str(show))            
-    return dir
-
-def GetGenres(content):
-    distinct = []
-    for show in content:
-        for genre in show['genres']:
-            if genre not in distinct:
-                distinct += [ genre ]
-    return distinct
-    
-def GetChannels(content):
-    distinct = []
-    for show in content:
-        for channel in show['channels']:
-            if channel not in distinct:
-                distinct += [ channel ]
-    return distinct
-    
 def GetContent():
-    x = JSON.ObjectFromURL("http://www.sbs.com.au/api/video_feed/f/dYtmxB/section-programs?form=json")
+    x = JSON.ObjectFromURL("http://www.sbs.com.au/api/video_feed/f/dYtmxB/section-programs?form=json&q=world%20news%20australia")
     content = []
     for entry in x['entries'] :
-        show = {}
-        Log("Found show" + entry['title'])
-        try:
-            show['thumbnailURL']=entry['media$thumbnails'][0]['plfile$downloadUrl']
-        except:
-            Log("Failed to get thumbnail URL for " + entry['title'])
-            
-        #show['ID'] = entry['id'][-10:]
-        show['ID'] = re.search('.*/([0-9]+$)',entry['id']).group(1)
-        genres = []
-        channels = []            
-        for j in entry['media$categories']:
-            try:
-                if j['media$scheme'] == 'Genre':
-                    genres.append(str(j['media$name']))
-                if j['media$scheme'] == 'Channel':
-                    if j['media$name'] != 'Channel':
-                        channels.append(str(j['media$name']))
-            except:
-                Log("skipping category")
-        show['genres'] = genres
-        show['channels'] = channels
+        episode = {}
+        Log("Found episode" + entry['title'])
         for k in entry['media$content']: 
             try: 
                 if k['plfile$bitrate'] == 1500000:
-                    show['duration'] = k['plfile$duration']
-                    show['url'] = k['plfile$downloadUrl']
+                    episode['duration'] = k['plfile$duration']
+                    episode['url'] = k['plfile$downloadUrl']
             except: 
                 Log("No 1.5K bitrate found")
         try:
-            show['rating'] = entry['media$ratings'][0]['rating']
+            episode['rating'] = entry['media$ratings'][0]['rating']
         except:
             Log("Couldn't get rating")
-        show['programName'] = entry['pl1$programName']
-        show['name'] = entry['title']
-        show['description'] = entry['description']
-        content.append(show)
+        episode['name'] = entry['title']
+        episode['description'] = entry['description']
+        episode['thumbnailURL'] = entry['plmedia$defaultThumbnailUrl']
+        dir.Append(WebVideoItem(episode['url'], title=episode['name'], subtitle='runtime: '+ str(int(episode['duration']/60)) +' mins.', thumb=episode['thumbnailURL'], summary=episode['description']))
+    content.append(episode)
     return content    
